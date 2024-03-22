e3c71f4bed17adbed622cac12af513d0baa90d3a
hemali-kacha <143687370+hemali-kacha@users.noreply.github.com>
2024-03-22 13:09:38 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar';
+// images
+import myImage from './images/feed.png';
+import myImage2 from './images/explore.png';
+import myImage3 from './images/favourite.png';
+import myImage4 from './images/group1.png';
+import myImage5 from './images/favourite.png';
+import myImage6 from './images/message.png';
+import myImage7 from './images/setting.png';
 
 
 function App(props) {

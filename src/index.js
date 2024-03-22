873628992347db48e9d32dc78d757d435d124db4
hemali-kacha <143687370+hemali-kacha@users.noreply.github.com>
2024-03-22 13:10:50 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -2,27 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Navbar from './Navbar';
+
 import reportWebVitals from './reportWebVitals';
-// images
-import myImage from './images/feed.png';
-import myImage2 from './images/explore.png';
-import myImage3 from './images/favourite.png';
-import myImage4 from './images/group1.png';
-import myImage5 from './images/favourite.png';
-import myImage6 from './images/message.png';
-import myImage7 from './images/setting.png';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar text="Feed" img={myImage}/>
-    <Navbar text="Explore" img={myImage2}/>
-    <Navbar text="MarketPlace" img={myImage3}/>
-    <Navbar text="Groups" img={myImage4}/>
-    <Navbar text="My Favorites" img={myImage5}/>
-    <Navbar text="Messages" img={myImage6}/>
-    <Navbar text="Settings" img={myImage7}/>
+   <App/>
     
   </React.StrictMode>
 );

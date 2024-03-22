092ef7dc5fff5e5a12e5e4826d0e660cb707b374
hemali-kacha <143687370+hemali-kacha@users.noreply.github.com>
2024-03-22 13:09:06 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import './App.css';
 function App(props) {
   return (
     <>
-        
+        <Navbar text="Feed" img={myImage}/>
+            <Navbar text="Explore" img={myImage2}/>
+            <Navbar text="MarketPlace" img={myImage3}/>
+            <Navbar text="Groups" img={myImage4}/>
+            <Navbar text="My Favorites" img={myImage5}/>
+            <Navbar text="Messages" img={myImage6}/>
+            <Navbar text="Settings" img={myImage7}/>
     </>
   );
 }

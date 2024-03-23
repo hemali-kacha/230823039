d3b5c84c99dda56bf7e5c03a24d2893c5850551b
hemali-kacha <143687370+hemali-kacha@users.noreply.github.com>
2024-03-23 10:12:12 +0530

    Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,7 @@ function Navbar(props) {
     <>
         <div>
           <p style={{"paddingBottom":"15px","width":"30%"}}>
-            <img src={props.img} alt="Myimage" style={{"height":"30px","width":"40px","float":"left","marginRight":"15px","marginBottom":"0"}}/>
+            <img src={props.img} alt="Myimage" style={{"height":"30px","width":"40px","float":"left","marginRight":"15px","marginBottom":"0" ,"paddingRight":"20px"}}/>
             <span style={{"color":"black"}}>{props.text}</span>
             <br></br>
           </p>

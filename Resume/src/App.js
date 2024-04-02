0d1bce037555be1f93a74601cf99482f0677d25b
unknown <hemalikacha36@gmail.com>
2024-04-02 13:49:59 +0530

    Resume Template

--- Resume/src/App.js
+++ Resume/src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Resume from './Resume';
+
+const abc=[
+  {
+    "title":"Ishan Agraval"  
+  }
+]
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Sign in</h1>
-     <form>
-      <label for="">Name</label>
-      <input type="text"></input>
-      
-      <label for="">Email</label>
-      <input type="text"></input>
-      <label for="">Password</label>
-      <input type="password"></input>
-      <button type="Submit">Submit</button>
-     </form>
-      
-    </div>
+    <>
+    <Resume title="Ishan Agraval"/>
+    </>
   );
 }
 
